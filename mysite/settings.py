9c79efa1760b85977bbb8ed06369de82b24bf3a2
Victor Barros <victor.barros@windowslive.com>
2017-08-28 01:04:36 -0300

    Add ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'qe=xn06phh#is79y7z%-wwvm(^c+wxr*1n0q744@_@8lqug-tm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['udacityblogproject.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
